fb91ca63ba21830255dc4aef6cfa27e652ad4e8a
Tasin1025 <tasintt@gmail.com>
2023-09-18 01:17:52 +0600

    all functions done

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook,faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -11,7 +13,7 @@ function Home() {
     const [totalCreditHour, setTotalCreditHour] = useState(0);
     const [totalPrice, setTotalPrice] = useState(0);
 
-    
+
     useEffect(() => {
         fetch('courses.json')
             .then(res => res.json())
@@ -20,24 +22,23 @@ function Home() {
 
     const handleSelectCourse = (course) => {
         const newTotalCreditHour = totalCreditHour + course.credit_hour;
-    
+
         if (newTotalCreditHour > 20) {
-            toast.error("Maximum is 20 credit hours.");  // This is where we show the toast.
-            return; // Return early to prevent further logic.
+            toast.error("Maximum is 20 credit hours");
+            return;
         }
-    
+
         setSelectedCourses([...selectedCourses, course]);
         setTotalCreditHour(newTotalCreditHour);
         setTotalPrice(prevTotalPrice => prevTotalPrice + course.price);
     };
-    
+
 
     const isCourseSelected = (course) => {
         return selectedCourses.includes(course);
     };
 
     return (
-        
         <div>
             <ToastContainer position="top-right" />
             <div className=" flex" >
@@ -45,35 +46,37 @@ function Home() {
                     <h1 className='text-2xl text-center px-5 font-bold pt-6 pb-6'> Course Registration</h1>
                     <div className="cards-container grid grid-cols-3 gap-3 ">
                         {
-                        courses.map(course => (
-                            <div key={course.id} className="card bg-base-100 shadow-xl m-3 p-3 flex flex-col justify-center items-center ">
-                                <figure>
-                                    <img src={course.image} alt="Course" />
-                                </figure>
-                                <div className="card-body p-3">
-                                    <h2 className="card-title font-bold text-2xl">{course.title}</h2>
-                                    <p className="text-gray-400">{course.description}</p>
-                                    <p className="text-2xl text-gray-500">Price : {course.price} Credit : {course.credit_hour} </p>
+                            courses.map(course => (
+                                <div key={course.id} className="card bg-base-100 shadow-xl m-3 p-3 flex flex-col justify-center items-center ">
+                                    <figure>
+                                        <img src={course.image} alt="Course" />
+                                    </figure>
+                                    <div className="card-body p-3">
+                                        <h2 className="card-title font-bold text-2xl"> {course.title} </h2>
+                                        <p className="text-gray-400">{course.description}</p>
+                                        <p className="text-2xl text-gray-500"> 
+                                        <FontAwesomeIcon icon={faDollarSign} /> 
+                                         Price : {course.price} <FontAwesomeIcon icon={faBook} /> Credit : {course.credit_hour} </p>
 
-                                    <div className="card-actions justify-center">
-                                        {isCourseSelected(course) ? (
-                                            <button
-                                                className="btn btn-primary w-full rounded-md bg-slate-400 text-white p-3 mt-2" disabled
-                                            >
-                                                Selected
-                                            </button>
-                                        ) : (
-                                            <button
-                                                className="btn btn-primary w-full rounded-md bg-[#2F80ED] text-white p-3 mt-2"
-                                                onClick={() => handleSelectCourse(course)}
-                                            >
-                                                Select
-                                            </button>
-                                        )}
+                                        <div className="card-actions justify-center">
+                                            {isCourseSelected(course) ? (
+                                                <button
+                                                    className="btn btn-primary w-full rounded-md bg-slate-400 text-white p-3 mt-2" disabled
+                                                >
+                                                    Selected
+                                                </button>
+                                            ) : (
+                                                <button
+                                                    className="btn btn-primary w-full rounded-md bg-[#2F80ED] text-white p-3 mt-2"
+                                                    onClick={() => handleSelectCourse(course)}
+                                                >
+                                                    Select
+                                                </button>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
+                            ))
                         }
                     </div>
                 </div>
@@ -84,11 +87,11 @@ function Home() {
                         <h2 className="mb-3 font-semibold text-xl border-b-3">Course Name</h2>
                         <ol>
                             {selectedCourses.map((course, index) => (
-                                <li key={index}>{course.title}</li>
+                                <li key={index}>{index+1}.  {course.title}</li>
                             ))}
                         </ol>
                         <h2 className="mb-3 font-semibold text-xl border-b-2">Total Credit Hour: {totalCreditHour}</h2>
-                    
+
                         <h2 className="mb-3 font-semibold text-xl">Total Price: ${totalPrice}</h2>
                     </div>
                 </div>
